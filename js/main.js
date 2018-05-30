2ae911e30d34b779be6582d2fef219eb94f4066d
Annie Ahnn <anniebert@Annies-MacBook-Pro.local>
2018-05-30 10:17:19 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+
+var flipCard = function (cardId){
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+ if (cardsInPlay.length === 2) {
+ checkForMatch();
+}
+}
+
+flipCard(0);
+flipCard(2);
